45b429daa66ceac433c18dbdf798e449e1c69b99
thanharafeek <fathimathanha37@gmail.com>
2023-01-12 14:25:18 +0530

    first commit

---
+++ railway/account/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def log(request):
+    return render(request,"login.html")
+
+def register(request):
+    return render(request,"register.html")
+    

fdca4692a43c7f76a4982d6fef82b36afb0fa488
najiya817 <najiyanaji817@gmail.com>
2023-01-16 14:28:03 +0530

    monday16

--- railway/account/views.py
+++ railway/account/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def log(request):
-    return render(request,"login.html")
+    if request.method=="GET":
+        return render(request,"login.html")
+    elif request.method=="POST":
+        #print(request.POST)
+        print(request.POST.get("uname"))
+        print(request.POST.get("password"))
+        return HttpResponse("post request reached")
 
 def register(request):
     return render(request,"register.html")

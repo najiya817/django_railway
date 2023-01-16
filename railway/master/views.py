a27b8a84115d60be4c73e8d573e668a4dde8462d
najiya817 <najiyanaji817@gmail.com>
2023-01-16 13:35:26 +0530

    mndy16

--- railway/master/views.py
+++ railway/master/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     name="NASRIN"
-    return render(request,"home.html",{"data":name})
+    lst=["apple","orange","mango"]
+    return render(request,"home.html",{"data":name,"list":lst})
